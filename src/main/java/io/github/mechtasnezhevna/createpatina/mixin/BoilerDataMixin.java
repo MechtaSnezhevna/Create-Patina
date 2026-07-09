7cc8d2812c92e37ea0385e61fa800ffdc74c25ec
akagi <220655952+MechtaSnezhevna@users.noreply.github.com>
2026-07-09 14:39:28 +0800

    engine can now use on boiler

---
+++ src/main/java/io/github/mechtasnezhevna/createpatina/mixin/BoilerDataMixin.java
@@ -0,0 +1,29 @@
+package io.github.mechtasnezhevna.createpatina.mixin;
+
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.content.fluids.tank.BoilerData;
+import com.simibubi.create.content.kinetics.steamEngine.SteamEngineBlock;
+import com.tterrag.registrate.util.entry.BlockEntry;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(value = BoilerData.class, remap = false)
+public abstract class BoilerDataMixin {
+
+    @Redirect(
+            method = "evaluate",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/tterrag/registrate/util/entry/BlockEntry;has(Lnet/minecraft/world/level/block/state/BlockState;)Z"
+            ),
+            remap = false
+    )
+    private boolean redirectHas(BlockEntry<?> entry, BlockState state) {
+        if (entry == AllBlocks.STEAM_ENGINE) {
+            return state.getBlock() instanceof SteamEngineBlock;
+        }
+        return entry.has(state);
+    }
+}
