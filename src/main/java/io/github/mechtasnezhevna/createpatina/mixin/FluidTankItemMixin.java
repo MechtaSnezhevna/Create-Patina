0e1e96a1ef26a8092548a9d98156a4f4da2b9696
akagi <220655952+MechtaSnezhevna@users.noreply.github.com>
2026-07-20 15:46:53 +0800

    tank variants now can multiplace 4 or 9 blocks to make a layer of a big tank

---
+++ src/main/java/io/github/mechtasnezhevna/createpatina/mixin/FluidTankItemMixin.java
@@ -0,0 +1,35 @@
+package io.github.mechtasnezhevna.createpatina.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.simibubi.create.content.fluids.tank.FluidTankItem;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.BlockGetter;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(FluidTankItem.class)
+public abstract class FluidTankItemMixin {
+
+    @WrapOperation(
+            method = "tryMultiPlace",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/simibubi/create/api/connectivity/ConnectivityHandler;partAt(Lnet/minecraft/world/level/block/entity/BlockEntityType;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/entity/BlockEntity;"
+            )
+    )
+    private BlockEntity create_weathered_tanks$wrapPartAt(BlockEntityType<?> type, BlockGetter level, BlockPos pos, Operation<BlockEntity> original) {
+        BlockState placedOnState = level.getBlockState(pos);
+        Block placedOnBlock = placedOnState.getBlock();
+        Block currentBlock = ((BlockItem)(Object)this).getBlock();
+        if (currentBlock != placedOnBlock) {
+            return null;
+        }
+        return original.call(type, level, pos);
+    }
+}
